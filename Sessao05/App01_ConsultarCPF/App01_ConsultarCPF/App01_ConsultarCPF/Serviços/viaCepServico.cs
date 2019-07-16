a865371999fbd42ec34be18ed832b829af30f20b
VictordaSiIva <victor10019@hotmail.com>
2019-07-16 10:56:46 -0300

    App01 - Criado

---
+++ Sessao05/App01_ConsultarCPF/App01_ConsultarCPF/App01_ConsultarCPF/Serviços/viaCepServico.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using App01_ConsultarCPF.Serviços.Modelo;
+using Newtonsoft.Json;
+
+namespace App01_ConsultarCPF.Serviços
+{
+    public class viaCepServico
+    {
+        private static string EnderecoURL = "http://viacep.com.br/ws/{0}/json";
+
+        public static Endereco BuscarEnderecoviaCep(string cep)
+        {
+            string NovoEnderecoURL = string.Format(EnderecoURL, cep);
+            WebClient wc = new WebClient();
+            string conteudo = wc.DownloadString(NovoEnderecoURL);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(conteudo);
+
+            if (end.cep == null) return null;
+            return end;
+        }
+    }
+
+}
